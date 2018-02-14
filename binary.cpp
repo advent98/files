efa19176ec2ff48228a3a54da89373b94e64b278
advent98 <advent98@users.noreply.github.com>
2018-02-14 11:39:34 -0400

    files

---
+++ binary.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+ 
+   cout<<a<<endl;
+  cout<<b<<endl;
+   cout<<c<<endl;//display data to console (i.e. as formatted chars)
+
+   cout<<endl<<endl;
+
+ofstream myfile{"abc.bin, ios::binary"};
+
+	if(myfile)
+	{
+
+
+	myfile.write(reinterpret_cast<char*>(&a),sizeof(a));
+
+	myfile.write(reinterpret_cast<char*>(&a),sizeof(b));
+	
+	myfile.write(reinterpret_cast<char*>(&a),sizeof(c));
+	
+ }else
+
+	cout<<"error opening abc.bin";
+	myfile.close();
+
+ 
+   return 0;
+}
